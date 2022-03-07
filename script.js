dd2027f21e571332ed2617ddc4c6c021e0460daa
Piero Caceres <pierocaceres92@yahoo.com>
2022-03-07 15:25:52 -0500

    createGrid function generates the squares for the board

---
+++ script.js
@@ -0,0 +1,18 @@
+createGrid = () => {
+    let playArea = document.querySelector(`.play-area`)
+    let grid = 5
+
+    playArea.style.gridTemplateRows = `repeat(${grid}, 1fr)`
+    playArea.style.gridTemplateColumns = `repeat(${grid}, 1fr)`
+
+    for(let i = 0; i < grid*grid; i++){
+        let sqr = document.createElement('div')
+        sqr.setAttribute(`class`, `square`)
+        sqr.setAttribute(`id`, `${i}`)
+        //sqr.style.borderWidth = `1px thin black`
+        sqr.innerText = i
+        playArea.appendChild(sqr)
+    }     
+}
+
+createGrid()
