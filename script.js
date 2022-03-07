5d4644b9e188136ed72bc61f19b66b11bf431938
Piero Caceres <pierocaceres92@yahoo.com>
2022-03-07 16:56:09 -0500

    placeShips function was created. No functionality yet

--- script.js
+++ script.js
@@ -3,13 +3,22 @@ class Ship {
         this.location
         this.sunk = false
     }
-    sunkValue(){
+    getSunkValue(){
         return this.sunk
     }
-    sunk(){
+    setLocation(value){
+        this.location = value
+    }
+    setSunk(){
         this.sunk = true
     }
 }
+
+let fleetSize = 5
+let playerBoard = []
+let playerShips = []
+let computerBoard = []
+let playArea = document.querySelector('.play-area')
 
 createGrid = () => {
     let playArea = document.querySelector(`.play-area`)
@@ -22,10 +31,31 @@ createGrid = () => {
         let sqr = document.createElement('div')
         sqr.setAttribute(`class`, `square`)
         sqr.setAttribute(`id`, `${i}`)
-        //sqr.style.borderWidth = `1px thin black`
-        sqr.innerText = i
         playArea.appendChild(sqr)
     }     
 }
 
-createGrid()
+startGame = () => {
+    createGrid()
+
+    //Player sets his ships on the board
+    placeShips()
+}
+
+placeShips = () => {
+    let counter = 0
+    let ship1 = new Ship()
+    let ship2 = new Ship()
+    let ship3 = new Ship()
+    let ship4 = new Ship()
+    let ship5 = new Ship()
+    playerShips = [ship1, ship2, ship3, ship4, ship5]
+    console.log(playerShips)
+}
+
+startGame()
+
+playArea.addEventListener('click', (zone) => {
+    playerShips[counter].setLocation(zone.target.getAttribute('id'))
+    counter++
+})
