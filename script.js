88f26b455dcbb89c8fb4121cc143ceec93f3cce0
Piero Caceres <pierocaceres92@yahoo.com>
2022-03-08 10:27:41 -0500

    randomClick function places the computer ship on the grid

--- script.js
+++ script.js
@@ -20,11 +20,12 @@ class Ship {
 let counter = 0
 let fleetSize = 5
 let grid = 5
+let player1Turn = true
 let shipsPlaced = false
 let playerBoard = []
+let computerBoard = []
 let playerShips = []
 let computerShips = []
-let computerMoves = []
 let playArea = document.querySelector('.play-area')
 
 const createGrid = () => {
@@ -48,23 +49,22 @@ const player2Grid = () => {
 
     sideBar.style.gridTemplateRows = `repeat(${grid}, 1fr)`
     sideBar.style.gridTemplateColumns = `repeat(${grid}, 1fr)`
-    console.log(document.querySelectorAll(`.square`))
 
     for(let i = 0; i < grid*grid; i++){
         let sqr = document.createElement('div')
         sqr.setAttribute(`class`, `square`)
         sqr.setAttribute(`id`, `c${i}`)
         sideBar.appendChild(sqr)
-        computerMoves.push(i)
+        computerBoard.push(i)
     }
 }
 
 const startGame = () => {
     createGrid()
-    placeShips()
+    createShips()
 }
 
-const placeShips = () => {
+const createShips = () => {
     let p1Ship1 = new Ship()
     let p1Ship2 = new Ship()
     let p1Ship3 = new Ship()
@@ -75,17 +75,20 @@ const placeShips = () => {
     let p2Ship3 = new Ship()
     let p2Ship4 = new Ship()
     let p2Ship5 = new Ship()
+    
     playerShips = [p1Ship1, p1Ship2, p1Ship3, p1Ship4, p1Ship5]
     computerShips = [p2Ship1, p2Ship2, p2Ship3, p2Ship4, p2Ship5]
 }
 
 const randomClick = () => {
-    let randomIndex = Math.floor(Math.random()*computerMoves.length)
-    let position = computerMoves[randomIndex]
-    computerMoves.splice(randomIndex, 1)
-
     if(!shipsPlaced){
-        
+        computerShips.forEach((ship) => {
+            let randomIndex = Math.floor(Math.random()*computerBoard.length)
+            let position = computerBoard[randomIndex]
+            computerBoard.splice(randomIndex, 1)
+            ship.setLocation(position)
+        })
+        computerShips
     }
 }
 
@@ -93,7 +96,6 @@ startGame()
 
 playArea.addEventListener('click', (zone) => {
     if(!shipsPlaced){
-        console.log(playerShips[counter])
         playerShips[counter].setLocation(zone.target.getAttribute('id'))
         zone.target.style.background = '#385170'
         counter++
@@ -101,11 +103,8 @@ playArea.addEventListener('click', (zone) => {
             console.log(playerShips)
             player2Grid()
             //Computer randomly chooses square to place ships
+            randomClick()
             shipsPlaced = true
         }
     }
 })
-
-playerShips[0].setLocation(2)
-console.log(playerShips[0].getLocation())
-console.log(computerShips[0].getLocation())
