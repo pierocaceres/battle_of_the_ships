8b535ee7f74a4ffe39c6cfdd922551a9cfde24ce
Piero Caceres <pierocaceres92@yahoo.com>
2022-03-11 12:06:27 -0500

    audio controls display to none

--- scriptHomepage.js
+++ scriptHomepage.js
@@ -1,12 +1,4 @@
-// let audio = new Audio(`Media/keyboard_audio.m4a`)
-
-// window.onload = function() {
-//     audio.controls = true
-//     audio.autoplay
-// }
-
-// let audio = document.querySelector(`audio`)
-
-// window.onload = function() {
-//     audio.controls = false
-// }
+let audio = document.querySelector(`audio`)
+//audio.style.opacity = `0`
+audio.style.display = `none`
+audio.play()
